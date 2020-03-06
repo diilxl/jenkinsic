26242f73a80ea979e938beb60beca4648cf468ed
diilxl <diilxl@163.com>
2020-03-06 11:16:32 +0800

    添加了新的类

--- src/main/java/com/jenkins/ic/jenkinsic/JenkinsicApplication.java
+++ src/main/java/com/jenkins/ic/jenkinsic/JenkinsicApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsicApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("Hello World JAVA");
         SpringApplication.run(JenkinsicApplication.class, args);
     }
 
---
+++ src/main/java/com/jenkins/ic/jenkinsic/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.jenkins.ic.jenkinsic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author TODO
+ * @Description (这里用一句话描述这个方法的作用)
+ * @Classname HelloController
+ * @create 2020-03-06 11:08
+ * @UpdateUser TODO
+ * @UpdateDate 2020-03-06 11:08
+ **/
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return  "hello world quick";
+    }
+}
