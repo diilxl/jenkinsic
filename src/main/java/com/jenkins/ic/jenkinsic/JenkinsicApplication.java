f516dc98118379245eb1a48a465291827095178b
diilxl <diilxl@163.com>
2020-03-06 10:54:30 +0800

    修改提交springboot项目

---
+++ src/main/java/com/jenkins/ic/jenkinsic/JenkinsicApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.ic.jenkinsic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jenkins/ic/jenkinsic/JenkinsicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.ic.jenkinsic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
