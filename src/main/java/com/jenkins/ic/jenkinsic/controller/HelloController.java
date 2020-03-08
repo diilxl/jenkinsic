385f58ed9d72749e59d2f06394576ba751e19df6
diilxl <diilxl@163.com>
2020-03-08 17:00:30 +0800

    github钩子程序测试20203822

--- src/main/java/com/jenkins/ic/jenkinsic/controller/HelloController.java
+++ src/main/java/com/jenkins/ic/jenkinsic/controller/HelloController.java
@@ -19,6 +19,6 @@ public class HelloController {
 
     @RequestMapping("/diilxl")
     public String hello(){
-        return  "hello world quick1 ！！！！";
+        return  "hello world quick1 ？？？！";
     }
 }
